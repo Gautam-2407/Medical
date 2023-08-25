f101818288d54af8176f227cf89619e028068762
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-25 17:13:22 +0530

    backend start

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const PORT = 5000;
-
-app.get("/")
+const cors= require("cors");
+require("./db/connection");
+const router = require("./routes/router");
+const PORT = 5000; 
+app.use(express.json());
+app.use(cors());
+app.use("/", router);
 
 app.listen(PORT, () => {
     console.log("server listening on port: " + PORT);
