361f7cfbaf999cc7c892ddbd219cdb4a3507dd07
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-26 12:35:34 +0530

    drSchema made

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -11,7 +11,7 @@ exports.userregister = async (req, res) => {
       const preuser = await users.findOne({phone:phone});
 
       if (preuser){
-        return   res.status(200).send("User already exist")
+        return   res.status(200).json("User already exist")
       }
       else{
         const newuser = new users({
@@ -35,35 +35,25 @@ exports.userregister = async (req, res) => {
   
   //CHECK PHONE INFORMATION IN DATABASE
   exports.userlogin = async (req, res) => {
-    // const { username, password } = req.body;
+     const { phone } = req.body;
   
-    // const user = await Stream_ui.findOne({ username: username });
-    // if (!user) {
-    //   return res.send("Username not found")
-    // }
-  
-    // try {
-    //   const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-    //   if (isPasswordValid) {
-        
-  
-    //     console.log("Valid");
-    //     const login_token = jwt.sign({username: user.username, id: user._id},
-    //       SecretKey);
-    //       res.status(201).json({ exists: true, username: user.username, token:login_token});
-  
-    //   }
-    //   else {
-    //     res.status(201).json({ exists: false });
-    //     console.log("Password didn't Match");
-    //   }
-    // }
-    // catch (error) {
-    //   console.error("Error while querying MongoDB:", error);
-    //   res.status(500).json({ error: "Unable to connect with DB" });
-    // }
-  console.log("check");
+     const user = await users.findOne({ phone: phone });
+     
+     try{
+
+       if (!user) {
+         return res.send("Phone No. not found")
+        }
+        else {
+          res.status(201).json({ exists: true ,user});
+          console.log("Phone No. Match");
+        }
+      }
+    
+    catch (error) {
+      console.error("Error while querying MongoDB:", error);
+      res.status(500).json({ error: "Unable to connect with DB" });
+    }
     
   };
   
--- Server/routes/router.js
+++ Server/routes/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/userController");
+const drcontrollers = require("../controllers/drController");
 
 //Routes
 router.post("/user/login", controllers.userlogin);
 router.post("/user/register", controllers.userregister);
-
+router.post("/dr/login" , drcontrollers.drlogin);
+router.post("/dr/register" , drcontrollers.drregister);
 module.exports = router;
