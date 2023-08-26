e8aca957862e30a171a81073ebd041445f080e8d
Ramanhub <sainiraman8700@gmail.com>
2023-08-25 18:44:29 -0700

    some changes in login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,35 @@
 import React from 'react'
+// import { useState} from 'react';
+
 import './login.css'
 const Login = () => {
+
+    // const [username, setusername]=useState("");
+    // const [contact, setcontact]=useState("");
+
+    // const handle = async (e) => {
+    //      e.preventDefault();
+    //      console.log("button clicked");
+    //     try{
+    //         const response = await registerfunction()
+    //     }
+
+    // }
+
     return (
         <div className="login-container">
+
             <div className="login-wrapper">
                 <div className='login-left'>
                     <h1>Test</h1>
                 </div>
+                
 
                 <div className='login-right'>
+               
+                <div className="login-logo">
+                    <p>MedicalMaster</p>
+                </div>
                     <h2>Login Page</h2>
                     <h5>Welcomt to login page</h5>
                     <div className="input-field">
@@ -21,7 +42,7 @@ const Login = () => {
                         <input type="submit" value="Login with GooGle" />
                     </div>
                     <div className="button-field">
-                     <input type="submit"   name="submit" value="Login"  />
+                     <input type="submit"   name="submit" value="Login"   />
                      <p>Create your Account?</p>
                     </div>
         
