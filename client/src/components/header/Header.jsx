dc2b656efd26e4bfd878ba5be349de42eef67b59
Ramanwork24 <sainiraman8700@gmail.com>
2023-08-27 18:16:10 +0530

    some changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
    }
   
   return (
-       
+       <>
     <div className="header-container" id="word" >
       <div className="header-logo">
         <h1>Medical Magic</h1>
@@ -49,11 +49,58 @@ function Header() {
       <div className="header-end">
       <Bell size={20} color="#ffffff" strokeWidth={1.5} />
       </div>
-      <div className="header-end">
-      <User size={20} color="#ffffff" strokeWidth={1.5}  onClick={Bgchange}/>
+      <div className="header-end-end">
+      <User size={20} color="#ffffff" strokeWidth={1.5}   onClick={Bgchange}/>
       </div>
       </div>
-    </div>
+      
+      </div>
+      <div className="headerone">
+        <div className="headerone-start">
+        <p>Check Your </p>
+        <h2>Overall Health</h2>
+        </div>
+        <div className="headerone-center">
+          <p>Month</p>
+          <h4>August</h4>
+        </div>
+        <div className="headerone-end">
+          <div className="headerone-end1">
+            <p>Sun</p>
+            <h4>01</h4>
+          </div>
+          <div className="headerone-end1">
+            <p>Mon</p>
+            <h4>02</h4>
+          </div>
+          <div className="headerone-end1">
+            <p>Tue</p>
+            <h4>03</h4>
+          </div>
+          <div className="headerone-end1">
+            <p>Web</p>
+            <h4>04</h4>
+          </div>
+          <div className="headerone-end1">
+            <p>Thu</p>
+            <h4>05</h4>
+          </div>
+          <div className="headerone-end1">
+            <p>Fri</p>
+            <h4>06</h4>
+          </div>
+          <div className="headerone-end1">
+            <p>Sat</p>
+            <h4>07</h4>
+          </div>
+        </div>
+      </div>
+      <div className="headertwo-container">
+        <div className="headertwo-start">
+          <p>Heart</p>
+        </div>
+      </div>
+      </>
   )
 }
 
---
+++ client/src/components/headerone/Headerone.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Headerone() {
+  return (
+    <div className="header">
+      
+    </div>
+  )
+}
+
+export default Headerone
