a284e33e9dd9767b941275562e29cefd995c1570
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-28 15:34:26 +0530

    merging

--- client/src/index.js
+++ client/src/index.js
@@ -8,16 +8,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-   
-    <App />
-   
-  </React.StrictMode>
-);
-=======
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
->>>>>>> c5234bf4d52a9da4f2fa8e07d27407187ca626a8
