d3cf95f5f13190691f4c90a2f34b5ba85b89a982
mavmodz <maverickjetman07@gmail.com>
2023-08-31 17:22:45 +0530

    header updated

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { LayoutDashboard, FileText, CalendarDays, MessageCircle, Settings, Phone, BellDot, User } from 'lucide-react';
 import './header.css';
 
 function Header() {
@@ -7,7 +8,7 @@ function Header() {
 
     const handleItemClick = (index) => {
         setActiveItem(index);
-      };
+    };
 
     return (
         <div className='header-container'>
@@ -15,20 +16,38 @@ function Header() {
                 <p>Test header</p>
                 <div className="nav-menu">
                     <ul className='nav-menu-list'>
-                        {['dashboard', 'dashboard', 'dashboard', 'dashboard', 'dashboard'].map((item, index) => (
+                        {[
+                            { text: 'Dashboard', icon: <LayoutDashboard strokeWidth={1.5} /> },
+                            { text: 'Reports', icon: <FileText strokeWidth={1.5} /> },
+                            { text: 'Calendar', icon: <CalendarDays strokeWidth={1.5} /> },
+                            { text: 'Chat', icon: <MessageCircle strokeWidth={1.5} /> },
+                            { text: 'Settings', icon: <Settings strokeWidth={1.5} /> }
+                        ].map((item, index) => (
                             <li
                                 key={index}
                                 className={`nav-menu-items ${index === activeItem ? 'active' : ''}`}
                                 onClick={() => handleItemClick(index)}
                             >
-                                {item}
+                                <div className="nav-item-content">
+                                    {item.icon}
+                                    {item.text}
+                                </div>
                             </li>
                         ))}
                     </ul>
                 </div>
                 <div className="nav-button-wrapper">
-                    <button type="button">login</button>
-                    <button type="button">Register</button>
+                    <div className="nav-button-list">
+                        <div className="nav-button-items">
+                            <Phone strokeWidth={1.5} size={20} />
+                        </div>
+                        <div className="nav-button-items">
+                            <BellDot strokeWidth={1.5} size={20} />
+                        </div>
+                        <div className="nav-button-items-active">
+                            <User strokeWidth={1.5} size={20} color="#ffff" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
